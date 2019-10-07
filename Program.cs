68a6fd1f5531fb224e67d3d2f60d893948a216f6
Brian Wied <Brian.Wied@gmail.com>
2019-10-07 08:52:31 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using FanoutExchange;
+using MassTransit;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace GettingStartedMassTransit
+{
+    public class YourMessage
+    {
+        public string Text { get; set; }
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var console = new PublisherConsole("", "", null);
+                var action = new Action<string, string, IBasicProperties, byte[]>(channel.BasicPublish);
+                console.StartPublisher(action);
+            }
+        }
+    }
+}
